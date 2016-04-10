7139cbc92040bc88063362b733b70d58f99f1078
Johan <bionicapps@gmail.com>
2016-04-10 14:45:12 -0700

    init Address Map feature

--- mobile/src/main/java/com/bionicapps/automactions/address/AddressMapActivity.java
+++ mobile/src/main/java/com/bionicapps/automactions/address/AddressMapActivity.java
@@ -1,7 +1,9 @@
 package com.bionicapps.automactions.address;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by johan on 4/10/16.
  */
-public class AddressMapActivity {
+public class AddressMapActivity extends AppCompatActivity {
 }
--- mobile/src/main/java/com/bionicapps/automactions/address/AddressMapFragment.java
+++ mobile/src/main/java/com/bionicapps/automactions/address/AddressMapFragment.java
@@ -1,7 +1,30 @@
 package com.bionicapps.automactions.address;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bionicapps.automactions.R;
+import com.google.android.gms.maps.MapView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 /**
  * Created by johan on 4/10/16.
  */
-public class AddressMapFragment {
-}
+public class AddressMapFragment extends Fragment {
+
+    @Bind(R.id.map_view)
+    private MapView mapView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_address_map, container, false);
+        ButterKnife.bind(this, view);
+
+        return view;
+    }
